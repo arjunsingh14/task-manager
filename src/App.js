84ad97a1e03c6fc5a4faf5db72cac5908e14056b
arjinsingh14 <arjunbhandal@Arjuns-MacBook-Air.local>
2022-05-02 15:23:57 +0800

    added localStorage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import "./App.scss";
 import Logobar from "./components/Logobar";
 import Form from "./components/Form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Tasks from "./components/Tasks";
-
 function App() {
-  const generateId = () => {
-    return new Date().getTime().toString() * Math.random();
-  };
+  const [tasks, setTasks] = useState(
+    JSON.parse(window.localStorage.getItem("tasks")) || []
+  );
 
-  const [tasks, setTasks] = useState([
-    { id: generateId(), taskName: "Wash the dishes", date: "2022-05-01" },
-    
-  ]);
+  useEffect(() => {
+    window.localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const removeTask = (id) => {
-    const remainingTasks = tasks.filter(elem => elem.id !== id);
+    const remainingTasks = tasks.filter((elem) => elem.id !== id);
     setTasks(remainingTasks);
-  }
-
+  };
 
   return (
     <div className="App">
       <Logobar />
-      <Form setTasks = {setTasks} tasks = {tasks}/>
-      <Tasks tasks={tasks} remove={removeTask}/>
+      <Form setTasks={setTasks} tasks={tasks} />
+      <Tasks tasks={tasks} remove={removeTask} />
     </div>
   );
 }
